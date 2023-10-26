c2ade8a188a296d53e83e276a0b5dfcac9f7d299
ReDay Zarra <redayzarra@gmail.com>
2023-10-26 04:09:10 -0400

    Styling the sidebar item the way I like it

--- app/(dashboard)/_components/SideBar/SideBarItem.tsx
+++ app/(dashboard)/_components/SideBar/SideBarItem.tsx
@@ -1,9 +1,8 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { LucideIcon } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
 
 interface SidebarItemProps {
   icon: LucideIcon;
@@ -29,12 +28,12 @@ export const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
       onClick={onClick}
       type="button"
       className={cn(
-        "flex items-center gap-x-2 ml-4 mb-1 rounded-l-xl text-primary/75 text-sm font-[500] pl-6 transition-all hover:text-primary hover:bg-primary/10",
+        "flex items-center gap-x-2 mx-4 mb-1 rounded-xl text-primary/75 text-sm font-[500] pl-9 transition-all hover:text-primary hover:bg-primary/10",
         isActive &&
           "text-secondary bg-primary hover:bg-primary hover:text-secondary"
       )}
     >
-      <div className="flex items-center gap-x-2 py-3 ">
+      <div className="flex items-center gap-x-3 py-2 ">
         <Icon size={22} className={cn(isActive && "text-secondary")} />
         {label}
       </div>
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ import SideBar from "./_components/SideBar/SideBar";
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
-      <div className="hidden md:flex h-full w-52 flex-col fixed inset-y-0 z-50">
+      <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
         <SideBar />
       </div>
       {children}
