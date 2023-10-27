af2ef92d7b7a24d9e0fb3aa06f4da5660ffd6910
ReDay Zarra <redayzarra@gmail.com>
2023-10-27 04:49:31 -0400

    Styling sidebar item the way I like it

---
+++ app/(dashboard)/_components/SideBarItem.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { LucideIcon } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+import React from "react";
+
+interface Props {
+  icon: LucideIcon;
+  label: string;
+  href: string;
+}
+
+const SideBarItem = ({ icon: Icon, label, href }: Props) => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const isActive =
+    (pathname === "/" && href === "/") ||
+    pathname === href ||
+    pathname?.startsWith(`${href}/`);
+
+  const onClick = () => {
+    router.push(href);
+  };
+
+  return (
+    <button
+      onClick={onClick}
+      type="button"
+      className={cn(
+        "flex items-center gap-x-2 mx-4 mb-2 rounded-xl text-muted-foreground text-sm font-[500] pl-9 transition-all hover:text-foreground/75 hover:bg-foreground/10",
+        isActive &&
+          "text-background bg-foreground hover:text-background hover:bg-foreground"
+      )}
+    >
+      <div className="flex items-center gap-x-2 py-2.5">
+        <Icon size={22} className={cn(isActive && "text-background")} />
+      </div>
+      {label}
+    </button>
+  );
+};
+
+export default SideBarItem;
--- app/(dashboard)/_components/SideBarRoutes.tsx
+++ app/(dashboard)/_components/SideBarRoutes.tsx
@@ -1,11 +1,37 @@
 "use client";
 
-import React from 'react'
+import React from "react";
+import SideBarItem from "./SideBarItem";
+import { Layout, Compass } from "lucide-react";
+
+const guestRoutes = [
+  {
+    icon: Layout,
+    label: "Dashboard",
+    href: "/",
+  },
+  {
+    icon: Compass,
+    label: "Browse",
+    href: "/search",
+  },
+];
 
 const SideBarRoutes = () => {
-  return (
-    <div>SideBarRoutes</div>
-  )
-}
+  const routes = guestRoutes;
 
-export default SideBarRoutes
+  return (
+    <div className="flex flex-col w-full">
+      {routes.map((route) => (
+        <SideBarItem
+          key={route.href}
+          icon={route.icon}
+          label={route.label}
+          href={route.href}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default SideBarRoutes;
