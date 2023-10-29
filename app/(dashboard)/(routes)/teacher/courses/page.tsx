5ec025eaf971440948f124b3ce29aa3d8a05ebde
ReDay Zarra <redayzarra@gmail.com>
2023-10-28 22:52:52 -0400

    Adding create new course page

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
 
 const CoursesPage = () => {
   return (
-    <div>CoursesPage</div>
-  )
-}
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button>New Course</Button>
+      </Link>
+    </div>
+  );
+};
 
-export default CoursesPage
+export default CoursesPage;
---
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from 'react'
+
+const CreatePage = () => {
+  return (
+    <div>CreatePage</div>
+  )
+}
+
+export default CreatePage
