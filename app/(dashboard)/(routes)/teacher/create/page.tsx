a844e01d397fe47d257ed5492c14483cc745f046
ReDay Zarra <redayzarra@gmail.com>
2023-11-10 16:35:15 -0500

    Creating formSchema using zod for course creation page

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,11 +1,31 @@
 "use client";
 
-import React from 'react'
+import React from "react";
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  title: z.string().min(1, {
+    message: "Title is required.",
+  }),
+});
 
 const CreatePage = () => {
-  return (
-    <div>CreatePage</div>
-  )
-}
+  return <div>CreatePage</div>;
+};
 
-export default CreatePage
+export default CreatePage;
