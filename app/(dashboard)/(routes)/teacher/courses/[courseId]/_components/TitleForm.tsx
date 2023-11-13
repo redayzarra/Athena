8081575a0284607dc505783eead21e65b8559d62
ReDay Zarra <redayzarra@gmail.com>
2023-11-13 09:26:15 -0500

    Finished setting up course title form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/TitleForm.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/TitleForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -28,6 +28,9 @@ const formSchema = z.object({
 });
 
 const TitleForm = ({ initialData, courseId }: Props) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const toggleEdit = () => setIsEditing((current) => !current);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: initialData,
@@ -42,11 +45,50 @@ const TitleForm = ({ initialData, courseId }: Props) => {
     <div className="mt-6 border bg-card rounded-md p-4">
       <div className="font-medium flex items-center justify-between">
         Course Title
-        <Button variant="ghost">
-          <PencilIcon className="h-4 w-4 mr-2" />
-          Edit
+        <Button onClick={toggleEdit} variant="ghost">
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <PencilIcon className="h-4 w-4 mr-2" />
+              Edit
+            </>
+          )}
         </Button>
       </div>
+      {isEditing ? (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 mt-4"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g 'Beginners Guide to Premiere Pro'"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <div className="flex items-center gap-x-2">
+              <Button disabled={isSubmitting} type="submit">
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      ) : (
+        <p className="text-sm mt-2">{initialData.title}</p>
+      )}
     </div>
   );
 };
