7b619e820181510871ba928cd259546a3a1ecd76
ReDay Zarra <redayzarra@gmail.com>
2023-11-13 07:51:04 -0500

    Completing layout for course completion page

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const CourseIdPage = ({ params }: { params: { courseId: string } }) => {
-  return <div>CourseIdPage: {params.courseId}</div>;
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+  const courseId = params.courseId;
+
+  // Checking user authentication
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/courses");
+  }
+
+  // Checking course validation
+  const course = await db.course.findUnique({
+    where: {
+      id: courseId,
+    },
+  });
+  if (!course) {
+    return redirect("/courses");
+  }
+
+  // Calculating the progress of form completion
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+  ];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+  const completionText = `(${completedFields}/${totalFields})`;
+
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">Course setup</h1>
+          <span className="text-sm text-muted-foreground">
+            Complete all fields {completionText}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CourseIdPage;
