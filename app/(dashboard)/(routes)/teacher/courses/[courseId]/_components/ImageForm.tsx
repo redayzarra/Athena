dde352b7bc0eef5f31d592f9c9823e6578020caf
ReDay Zarra <redayzarra@gmail.com>
2023-11-14 09:45:14 -0500

    Finishing image form component

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/ImageForm.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/ImageForm.tsx
@@ -3,14 +3,12 @@
 import FileUpload from "@/components/FileUpload";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { Course } from "@prisma/client";
 import axios from "axios";
 import { ImageIcon, PencilIcon, PlusCircle } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
 import * as z from "zod";
 
 interface Props {
@@ -28,17 +26,9 @@ const ImageForm = ({ initialData, courseId }: Props) => {
 
   const router = useRouter();
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      imageUrl: initialData?.imageUrl ?? "",
-    },
-  });
-
   // Initialize the useToast hook
   const { toast } = useToast();
 
-  const { isSubmitting, isValid } = form.formState;
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       await axios.patch(`/api/courses/${courseId}`, values);
@@ -48,14 +38,7 @@ const ImageForm = ({ initialData, courseId }: Props) => {
       });
       toggleEdit();
       router.refresh();
-    } catch (error) {
-      // toast({
-      //   title: "Something went wrong.",
-      //   description:
-      //     "There was a problem uploading your image. Make sure your image is less than 4MB.",
-      //   variant: "destructive",
-      // });
-    }
+    } catch (error) {}
   };
 
   return (
@@ -83,7 +66,7 @@ const ImageForm = ({ initialData, courseId }: Props) => {
         </Button>
       </div>
       {isEditing ? (
-        <div className="bg-background rounded-md">
+        <div className="bg-background rounded-md flex flex-col items-center justify-center">
           <FileUpload
             endpoint="courseImage"
             onChange={(url) => {
@@ -92,6 +75,9 @@ const ImageForm = ({ initialData, courseId }: Props) => {
               }
             }}
           />
+          <p className="text-xs text-muted-foreground">
+            16:9 aspect ratio recommended
+          </p>
         </div>
       ) : !initialData.imageUrl ? (
         <div className="flex items-center justify-center h-[190px] rounded-md bg-background">
