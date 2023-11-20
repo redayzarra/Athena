3c03ce43f2b5cd55077bcc57ea0e6a59813504d4
ReDay Zarra <redayzarra@gmail.com>
2023-11-20 17:05:03 -0500

    Upgrading basic data table

--- app/(dashboard)/(routes)/teacher/courses/_components/Columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/Columns.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import { formatDate } from "@/lib/formatDate";
 import { formatPrice } from "@/lib/formatPrice";
 import { Course as PrismaCourse, Category } from "@prisma/client";
 import { CellContext, ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
 import { FaRegCheckCircle, FaRegTimesCircle } from "react-icons/fa";
 
 // Extend the Course type to include the category object
@@ -13,29 +16,98 @@ type CourseWithCategory = PrismaCourse & {
 
 export const columns: ColumnDef<CourseWithCategory>[] = [
   {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        className="rounded-[0.25rem]"
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        className="rounded-[0.25rem]"
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
     accessorKey: "title",
-    header: "Title",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Title
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
     cell: (info: CellContext<CourseWithCategory, unknown>) => (
-      <span className="line-clamp-2 font-medium">{info.getValue() as string}</span>
+      <span className="line-clamp-2 font-medium">
+        {info.getValue() as string}
+      </span>
     ),
   },
   {
-    accessorFn: (row) =>
-      row.price === 0 || row.price === null ? "Free" : formatPrice(row.price),
+    accessorFn: (row) => row.price, // Return the actual numerical value
     id: "price",
-    header: "Price",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Price
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
     cell: (info: CellContext<CourseWithCategory, unknown>) => {
-      const value = info.getValue() as string;
-      return value === "Free" ? (
-        <span className="italic font-bold text-muted-foreground">{value}</span>
-      ) : (
-        <span className="font-bold">{value}</span>
+      // Use the raw value for display formatting
+      const priceValue = info.getValue() as number;
+      const displayValue =
+        priceValue === 0 || priceValue === null
+          ? "Free"
+          : formatPrice(priceValue);
+
+      return (
+        <div className="ml-4">
+          <span
+            className={
+              displayValue === "Free"
+                ? "italic font-bold text-muted-foreground"
+                : "font-bold"
+            }
+          >
+            {displayValue}
+          </span>
+        </div>
       );
     },
   },
   {
     accessorKey: "isPublished",
-    header: "Published",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Published
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
     cell: (info: CellContext<CourseWithCategory, unknown>) => {
       const isPublished = info.getValue() as boolean;
       return (
@@ -52,11 +124,36 @@ export const columns: ColumnDef<CourseWithCategory>[] = [
   {
     accessorFn: (row) => row.category?.name, // Access the category name
     id: "category",
-    header: "Category",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Category
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: (info: CellContext<CourseWithCategory, unknown>) => (
+      <span className="ml-4 line-clamp-1 font-medium">
+        {info.getValue() as string}
+      </span>
+    ),
   },
   {
     accessorFn: (row) => formatDate(row.updatedAt), // Format the date
     id: "updatedAt",
-    header: "Last Modified",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Last Modified
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
 ];
--- app/(dashboard)/(routes)/teacher/courses/_components/DataTable.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/DataTable.tsx
@@ -2,11 +2,19 @@
 
 import {
   ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import {
   Table,
   TableBody,
@@ -15,66 +23,270 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import { ChevronDown, Loader2 } from "lucide-react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useToast } from "@/components/ui/use-toast";
+import {
+  AlertDialogHeader,
+  AlertDialogFooter,
+} from "@/components/ui/alert-dialog";
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogCancel,
+  AlertDialogAction,
+} from "@radix-ui/react-alert-dialog";
+
+interface WithId {
+  id: string;
+}
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
 }
 
-export function DataTable<TData, TValue>({
+export function DataTable<TData extends WithId, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+  // State variables for sorting, filtering, etc.
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Initializing table
   const table = useReactTable({
     data,
     columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
   });
 
+  // Initialize router and toast
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const deleteSelectedCourses = async () => {
+    // Get the selected rows from the table
+    const selectedRows = table.getSelectedRowModel().rows;
+
+    // Extract the course IDs from the selected rows
+    const selectedCourseIds = selectedRows.map((row) => row.original.id);
+
+    try {
+      setIsLoading(true);
+      await Promise.all(
+        selectedCourseIds.map(
+          (courseId) => axios.delete(`/api/courses/${courseId}`) // API call to delete courses by ID
+        )
+      );
+
+      // Success
+      toast({
+        title: "Course Deleted",
+        description: "Selected course have been successfully deleted.",
+      });
+      router.refresh(); // Refresh to update the data table
+
+      // Error handling
+    } catch (error) {
+      toast({
+        title: "Something went wrong.",
+        description:
+          "Unable to delete the courses. Please check your connection and try again.",
+        variant: "destructive",
+      });
+    } finally {
+      setIsLoading(false);
+      setRowSelection({});
+    }
+  };
+
   return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
+    <div className="w-full">
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filter courses..."
+          value={(table.getColumn("title")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("title")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              Columns <ChevronDown className="ml-2 h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
                 return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
                 );
               })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="flex-1 text-sm text-muted-foreground">
+          {table.getFilteredSelectedRowModel().rows.length} of{" "}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
+        </div>
+        <div className="space-x-2">
+          {/* <Button
+            variant="outline"
+            size="sm"
+            onClick={deleteSelectedCourses}
+            disabled={Object.keys(rowSelection).length === 0}
+          >
+            {isLoading ? (
+              <>
+                <Loader2 className="animate-spin h-4 w-4 mr-2" />
+                Loading
+              </>
+            ) : (
+              "Delete"
+            )}
+          </Button> */}
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant="outline" size="sm">
+                Delete
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  your courses and remove your data from our servers.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction
+                  onClick={deleteSelectedCourses}
+                  disabled={Object.keys(rowSelection).length === 0}
+                >
+                  {isLoading ? (
+                    <>
+                      <Loader2 className="animate-spin h-4 w-4 mr-2" />
+                      Loading
+                    </>
+                  ) : (
+                    "Delete"
+                  )}
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
