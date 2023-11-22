51d0e9c6d928bc5ceddcc2a4b892ba79b54ca254
ReDay Zarra <redayzarra@gmail.com>
2023-11-22 06:06:12 -0500

    Building url to push for categories

--- app/(dashboard)/(routes)/search/_components/CategoryItem.tsx
+++ app/(dashboard)/(routes)/search/_components/CategoryItem.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import qs from "query-string";
 import React from "react";
 import { IconType } from "react-icons";
 
@@ -11,11 +13,38 @@ interface Props {
 }
 
 const CategoryItem = ({ label, icon: Icon, value }: Props) => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const currentCategoryName = searchParams.get("categoryName");
+  const currentTitle = searchParams.get("title");
+
+  const isSelected = currentCategoryName === label;
+
+  const onClick = () => {
+    const url = qs.stringify(
+      {
+        url: pathname,
+        query: {
+          title: currentTitle,
+          categoryName: isSelected ? null : label,
+        },
+      },
+      { skipNull: true, skipEmptyString: true }
+    );
+
+    router.push(url);
+  };
+
   return (
     <button
       className={cn(
         "py-2 px-3 text-sm font-medium border border-muted-foreground/20 dark:border-muted-foreground/15 rounded-full flex items-center gap-x-1 bg-muted-foreground/5 dark:hover:bg-card transition hover:shadow-md dark:shadow-muted-foreground/20"
+        // Change style when active
       )}
+      type="button"
+      onClick={onClick}
     >
       {Icon && <Icon size={20} />}
       <div className="truncate">{label}</div>
