d87c8a084b64d865e23e4e95d127345fa84b0cac
ReDay Zarra <redayzarra@gmail.com>
2023-12-01 18:02:49 -0500

    Setting up Strike webhook

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/CourseEnrollButton.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/CourseEnrollButton.tsx
@@ -1,17 +1,40 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
 import { formatPrice } from "@/lib/formatPrice";
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 interface Props {
   courseId: string;
   price: number;
 }
 
-const CourseEnrollButton = ({courseId, price }: Props) => {
+const CourseEnrollButton = ({ courseId, price }: Props) => {
+  const [isLoading, setIsLoading] = useState(false);
+  // Setup Toaster
+  const { toast } = useToast();
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.post(`/api/courses/${courseId}/checkout`);
+      window.location.assign(response.data.url);
+
+      // Error handling
+    } catch (error) {
+      toast({
+        title: "Something went wrong.",
+        description:
+          "Unable to enroll in the course. Please check your connection and try again.",
+        variant: "destructive",
+      });
+    } finally {
+      setIsLoading(true);
+    }
+  };
   return (
-    <Button size="sm" className="ml-4">
+    <Button size="sm" onClick={onClick} disabled={isLoading} className="ml-4">
       Enroll for {formatPrice(price)}
     </Button>
   );
