debe33370445efee9efb4c639865543b1d03fd2a
ReDay Zarra <redayzarra@gmail.com>
2023-12-04 17:11:33 -0500

    Fixing styling issue for card information

--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -28,47 +28,50 @@ const CourseCard = ({
 }: Props) => {
   return (
     <Link href={`/courses/${id}`}>
-      <div className="group hover:shadow-md dark:shadow-accent transition overflow-hidden border rounded-lg h-full">
+      <div className="group hover:shadow-md dark:shadow-accent transition overflow-hidden border rounded-lg h-full flex flex-col">
         {/* Image */}
-        <div className="relative w-full aspect-video rounded-t-md overflow-hidden">
+        <div className="relative w-full aspect-video rounded-t-md overflow-hidden flex-shrink-0">
           <Image fill className="object-cover" alt={title} src={imageUrl} />
         </div>
 
         {/* Info */}
-        <div className="flex flex-col p-2">
+        <div className="flex flex-col p-2 flex-grow">
           <div className="text-xl font-bold transition line-clamp-2">
             {title}
           </div>
           <p className="text-[14px] text-muted-foreground mt-1">{category}</p>
 
-          {/* Chapters and Progress Bar */}
-          <div className="my-3 gap-x-2 text-sm flex">
-            <div className="flex items-center gap-x-1 max-w-[200px]">
-              <BookOpen />
-              <span>
-                {chaptersLength} {chaptersLength === 1 ? "chapter" : "chapters"}
-              </span>
-              <LiaGripLinesVerticalSolid
-                size={20}
-                className="text-muted-foreground"
-              />
-            </div>
-            <div className="mt-2">
-              {progress !== null ? (
-                <CourseProgress
-                  size="sm"
-                  value={progress}
-                  variant={progress === 100 ? "success" : "default"}
+          {/* Align Chapters and Progress Bar at Bottom */}
+          <div className="mt-auto">
+            <div className="my-3 gap-x-2 text-sm flex justify-between items-center">
+              <div className="flex items-center gap-x-2 max-w-[200px]">
+                <BookOpen />
+                <span>
+                  {chaptersLength}{" "}
+                  {chaptersLength === 1 ? "chapter" : "chapters"}
+                </span>
+                <LiaGripLinesVerticalSolid
+                  size={20}
+                  className="text-muted-foreground"
                 />
-              ) : (
-                <p
-                  className={`font-semibold text-base inline-block ${
-                    price === 0 && "italic"
-                  }`}
-                >
-                  {price === 0 ? "Free" : formatPrice(price)}
-                </p>
-              )}
+              </div>
+              <div className="mr-12 mt-1.5">
+                {progress !== null ? (
+                  <CourseProgress
+                    size="sm"
+                    value={progress}
+                    variant={progress === 100 ? "success" : "default"}
+                  />
+                ) : (
+                  <p
+                    className={`font-semibold text-base inline-block ${
+                      price === 0 && "italic"
+                    }`}
+                  >
+                    {price === 0 ? "Free" : formatPrice(price)}
+                  </p>
+                )}
+              </div>
             </div>
           </div>
         </div>
